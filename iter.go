20ea81dd26aa3fc7f6c175c4f1b13a1c063726fb
deorth-kku <deorth@outlook.com>
2024-10-19 10:41:14 +0800

    add NewPair

--- iter.go
+++ iter.go
@@ -5,6 +5,10 @@ type Pair[K any, V any] struct {
 	Value V
 }
 
+func NewPair[K any, V any](Key K, Value V) Pair[K, V] {
+	return Pair[K, V]{Key: Key, Value: Value}
+}
+
 type PairSlice[K any, V any] []Pair[K, V]
 
 func (ps PairSlice[K, V]) Range(yield func(K, V) bool) {
