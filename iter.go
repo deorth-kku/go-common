0f148c95da1bac2d9cd7c341debef37656178083
deorth-kku <deorth@outlook.com>
2024-10-20 18:25:57 +0800

    safe range

--- iter.go
+++ iter.go
@@ -1,5 +1,7 @@
 package common
 
+import "iter"
+
 type Pair[K any, V any] struct {
 	Key   K
 	Value V
@@ -17,4 +19,18 @@ func (ps PairSlice[K, V]) Range(yield func(K, V) bool) {
 			return
 		}
 	}
+}
+
+func SafeRange[T any](f iter.Seq[T]) iter.Seq[T] {
+	if f == nil {
+		return func(func(T) bool) {}
+	}
+	return f
+}
+
+func SafeRange2[K any, V any](f iter.Seq2[K, V]) iter.Seq2[K, V] {
+	if f == nil {
+		return func(func(K, V) bool) {}
+	}
+	return f
 }
